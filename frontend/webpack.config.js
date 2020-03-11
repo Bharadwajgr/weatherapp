c10389b951ee1710d8ecb2420f6ba60ecc6263ab
bharadwaj <bharadwaj209@gmail.com>
2020-03-11 15:16:37 +0000

    changes made

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,64 +1,50 @@
 const webpack = require('webpack');
-const path = require('path');
-
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const GLOBALS = {
-  'process.env.ENDPOINT': JSON.stringify(process.env.ENDPOINT || 'http://0.0.0.0:9000/api'),
-};
-
 module.exports = {
-  mode: 'development',
-  cache: true,
-  devtool: 'cheap-module-eval-source-map',
-  entry: {
-    main: ['@babel/polyfill', path.join(__dirname, 'src/index.jsx')],
-  },
+  entry: './src/index.jsx',
   resolve: {
     extensions: ['.js', '.jsx'],
-    modules: [
-      'src',
-      'node_modules',
-    ],
   },
   devServer: {
     contentBase: 'src/public',
     historyApiFallback: true,
-    disableHostCheck: true,
-    host: process.env.HOST || '0.0.0.0',
-    port: process.env.PORT || 8000,
+    port: 8000,
+    host: '0.0.0.0',
+    hot: true,
   },
+  devtool: 'eval',
   output: {
-    filename: '[name].[hash:8].js',
+    filename: 'bundle.js',
     publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            '@babel/preset-react',
-            ['@babel/env', { targets: { browsers: ['last 2 versions'] }, modules: false }],
-          ],
-          plugins: [
-            '@babel/plugin-proposal-class-properties',
-          ],
-        },
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['react', 'es2016'] },
+        }],
       },
     ],
   },
+  watchOptions: {
+    poll: true,
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/public/index.html',
-      filename: 'index.html',
-    }),
+    new HtmlWebpackPlugin({ template: 'src/public/index.html' }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    new TransferWebpackPlugin([{ from: 'src/public' }], '.'),
-    new webpack.DefinePlugin(GLOBALS),
+    new TransferWebpackPlugin([
+      { from: 'src/public' },
+    ], '.'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        ENDPOINT: JSON.stringify(process.env.ENDPOINT || 'http://0.0.0.0:9000/api'),
+      },
+    }),
   ],
 };
